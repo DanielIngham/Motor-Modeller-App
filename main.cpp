88fd911ebfa1f1b9e6221ac2003423f91e31a711
Daniel Ingham <49901842+DanielIngham@users.noreply.github.com>
2024-12-09 09:38:00 +0200

    Add uniform spacing

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     serialReciever *m_thread = new serialReciever;  ///< Create new instance of serial reception class
     dataHandler *dh = new dataHandler;              ///< Create new instance of datahandler class
 
-    m_thread->startReceiver("COM4", 10000); ///< Start Serial Communication on COM4 with a timeout of 10 seconds.
+    m_thread->startReceiver("COM4", 10000);         ///< Start Serial Communication on COM4 with a timeout of 10 seconds.
 
     QObject::connect(m_thread, &serialReciever::sensorData, dh, &dataHandler::setLoadCellReading); ///< Connect sensorData signal from the serial receiver to the dataHanldlers slot
 
